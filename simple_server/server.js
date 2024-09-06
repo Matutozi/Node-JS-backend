97124cbf21ad0e883b5e2c97b308443bf6efa5b6
Matutozi <sobowalegz2@gmail.com>
2024-09-06 13:15:16 +0100

    first server

---
+++ simple_server/server.js
@@ -0,0 +1,10 @@
+const http = require("http");
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "content-Type": "text/plain" })
+    res.end("Hello World")
+
+})
+
+server.listen(port, () => console.log(`Server started listening on http://localhost:${port}`))
